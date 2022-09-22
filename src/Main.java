f0f81b448b8301ac89737e65d4579e551e2cabe6
PDemenkov <guapnik@gmail.com>
2022-09-22 14:55:03 +0300

    4 Задачи

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,22 @@
 public class Main {
     public static void main(String[] args) {
-
-
+        System.out.println("Задание 1");
+        for (int i = 1;i<=10;i++){
+            System.out.println(i);
+        }
+        System.out.println("Задание 2");
+        for (int i = 10; i>=1; i--){
+            System.out.println(i);
+        }
+        System.out.println("Задание 3");
+        for (int i = 0; i<=17; i++){
+            if (i%2==0){
+                System.out.println(i);
+            }
+        }
+        System.out.println("Задание 4");
+        for (int i=10;i>=-10;i--){
+            System.out.println(i);
+        }
     }
 }
